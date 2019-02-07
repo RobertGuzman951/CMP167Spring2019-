f15704b5bc2252b6b56b3ceddfbb96c59ffa3227
RobertGuzman951 <47428066+RobertGuzman951@users.noreply.github.com>
2019-02-07 12:49:39 -0500

    Add files via upload

---
+++ week2/AverageNumbers/src/Average.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/*
+ * Robert Guzman
+ * 2/7/2019
+ * Finding average of three numbers
+ */
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scnr = new Scanner(System.in);
+		double num1;
+		double num2;
+		double num3;
+		
+		scnr.nextDouble((num1+num2+num3)) /3);
+		System.out.println("Enter 3 Numbers:");
+	}
+
+}
